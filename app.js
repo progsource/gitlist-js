81519635fd6cd1e8510ad9523e413f0dbf3d8e26
M. Petra Baranski <info@progsource.de>
2014-01-06 21:04:58 +0100

    using now jade as template engine

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
+
+app.set('views', __dirname + '/app/views');
+app.set('view engine', 'jade');
+
 
 app.get('/', function(req, res) {
-    var body = 'Hello World';
+    var repositories = [];
+    var dir = 'C:/repo_git';
 
-    res.setHeader('Content-Type', 'text/plain');
-    res.setHeader('Content-Length', Buffer.byteLength(body));
-
-    res.end(body);
+    
+console.log(repositories);
+    res.render('index', { title: 'repository list'});
 });
 
 app.use(express.static(__dirname + '/app/public'));
