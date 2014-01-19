ce470fbc89a0568ed7e16230e41f3fcc91233302
M. Petra Baranski <info@progsource.de>
2014-01-19 10:45:39 +0100

    log view is now showing last 15 commits

--- app/modules/GitLog.js
+++ app/modules/GitLog.js
@@ -3,15 +3,25 @@
  */
 var GitLog = function() {
     var commits = {};
+    var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
     var commitStringToObject = function(data) {
 	var history = data.toString('utf-8').split('\n');
 	history.forEach(function(logLine, index) {
 	    if ('commit ' == logLine.substr(0, 'commit '.length)) {
-                commits[logLine.substr(7)] = {
-		    author: history[index + 1],
-		    date: history[index + 2],
-		    msg: history[index + 4]
+		var date = history[index + 2].substr(8);
+		var shortDate = new Date(Date.parse(date));
+		var dateTitle = months[shortDate.getMonth()] + ' ' + shortDate.getDate() + ', ' + shortDate.getFullYear();
+		if ('undefined' == typeof commits[dateTitle]) {
+	            commits[dateTitle] = {};
+		}
+                commits[dateTitle][logLine.substr(7)] = {
+		    author: {
+			name: history[index + 1].substring(8, history[index + 1].indexOf('<') - 1),
+		        email: history[index + 1].substring(history[index + 1].indexOf('<') + 1, history[index + 1].indexOf('>'))
+		    },
+		    date: history[index + 2].substr(8),
+		    msg: history[index + 4].substr(4)
 		};
 	    }
 	});
@@ -19,7 +29,7 @@ var GitLog = function() {
 
     this.getLog = function(path, branch, callback) {
 	var spawn = require('child_process').spawn;
-	var gitLog = spawn('git', ['log', '--first-parent', branch, '-n', 20], {cwd: path});
+	var gitLog = spawn('git', ['log', '--first-parent', branch, '-n', 15], {cwd: path});
 	gitLog.stdout.on('data', commitStringToObject);
 	gitLog.on('close', function(code) {
 	    callback({breadcrumb: {'Commit history': ''}, commits: commits});
