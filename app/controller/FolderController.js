dcda2c9c231c8eb3dd1ba38fdf7410d701564a4d
M. Petra Baranski <info@progsource.de>
2014-01-23 00:03:34 +0100

    folder controller line endings, tabs to spaces

--- app/controller/FolderController.js
+++ app/controller/FolderController.js
@@ -4,72 +4,72 @@
 var FolderController = function() {
     var getBootstrap = function() {
         var Bootstrap = require(__dirname + '/../Bootstrap.js');
-	return new Bootstrap().init();
+    return new Bootstrap().init();
     };
 
     var renderFolder = function(req, res, basePath, reponame, branch, dataPath) {
         var GitRepo = require(__dirname + '/../modules/GitRepo.js');
-	var gitRepo = new GitRepo();
+    var gitRepo = new GitRepo();
 
-	var GitData = require(__dirname + '/../modules/GitData.js');
-	var gitData = new GitData();
+    var GitData = require(__dirname + '/../modules/GitData.js');
+    var gitData = new GitData();
 
-	gitRepo.setCurrentReponame(reponame);
-	gitRepo.init();
-	
-	var branches = gitRepo.getBranches();
+    gitRepo.setCurrentReponame(reponame);
+    gitRepo.init();
+    
+    var branches = gitRepo.getBranches();
 
-	if (null === branch) {
+    if (null === branch) {
             branch = gitRepo.getCurrentBranch();
         } else {
-	    gitRepo.setCurrentBranch(branch);
-	}
+        gitRepo.setCurrentBranch(branch);
+    }
 
-	var renderIt = function(data) {
-	    res.render(
+    var renderIt = function(data) {
+        res.render(
                 'folderView',
                 {
-		    title: reponame,
-		    reponame: reponame,
-		    heads: branches.heads,
-		    tags: branches.tags,
-		    branch: branch,
-		    directoryContents: data.folder,
-		    breadcrumb: data.breadcrumb,
-		    activeTab: 'Files'
-		}
-	    );
-	};
+            title: reponame,
+            reponame: reponame,
+            heads: branches.heads,
+            tags: branches.tags,
+            branch: branch,
+            directoryContents: data.folder,
+            breadcrumb: data.breadcrumb,
+            activeTab: 'Files'
+        }
+        );
+    };
 
         gitData.getFolder(basePath + '/' + reponame, branch, dataPath, renderIt);
     };
 
     this.indexAction = function(req, res) {
-	var fs = require('fs');
+    var fs = require('fs');
         var basePath = getBootstrap().getBasePath();
         var reponame = req.params.reponame;
 
-	if (!fs.existsSync(basePath + '/' + reponame)) {
-	    res.send(404, 'File/Dir not found');
-	    return;
-	}
+    if (!fs.existsSync(basePath + '/' + reponame)) {
+        res.send(404, 'File/Dir not found');
+        return;
+    }
 
-	renderFolder(req, res, basePath, reponame, null, '.');
+    renderFolder(req, res, basePath, reponame, null, '.');
     };
 
     this.branchAction = function(req, res) {
         var basePath = getBootstrap().getBasePath();
-	var reponame = req.params.reponame;
-	var branch = req.params.branch;
+    var reponame = req.params.reponame;
+    var branch = req.params.branch;
 
         renderFolder(req, res, basePath, reponame, branch, '.');
     };
 
     this.treeAction = function(req, res) {
         var basePath = getBootstrap().getBasePath();
-	var reponame = req.params.reponame;
-	var branch = req.params.branch;
-	var dataPath = req.params.dir;
+    var reponame = req.params.reponame;
+    var branch = req.params.branch;
+    var dataPath = req.params.dir;
 
         renderFolder(req, res, basePath, reponame, branch, dataPath);
     };
