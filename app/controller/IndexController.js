4c13a093c98c024fca9961b3d947bd24ffd90cd2
M. Petra Baranski <info@progsource.de>
2014-01-23 00:13:58 +0100

    jsdoc in index controller

--- app/controller/IndexController.js
+++ app/controller/IndexController.js
@@ -1,28 +1,45 @@
 /**
  * Index Controller
+ *
+ * @file
+ * @license MIT
+ */
+
+/**
+ * @constructor
  */
 var IndexController = function() {
+    /**
+     * get bootstrap
+     *
+     * @return {object} bootstrap
+     */
     var getBootstrap = function() {
-    var Bootstrap = require('./../Bootstrap.js');
-    return new Bootstrap().init();
+        var Bootstrap = require('./../Bootstrap.js');
+        return new Bootstrap().init();
     };
 
+    /**
+     * index action
+     *
+     * @param {object} req - request object
+     * @param {object} res - response object
+     */
     this.indexAction = function(req, res) {
-    var fs = require('fs');
-    var repositories = {};
-    var basePath = getBootstrap().getBasePath();
-    var folder = fs.readdirSync(basePath);
+        var fs = require('fs');
+        var repositories = {};
+        var basePath = getBootstrap().getBasePath();
+        var folder = fs.readdirSync(basePath);
 
-    folder.forEach(function(currentDir) {
-        var stats = fs.statSync(basePath + '/' + currentDir);
+        folder.forEach(function(currentDir) {
+            var stats = fs.statSync(basePath + '/' + currentDir);
 
-        if (stats && stats.isDirectory()) {
-        var description = fs.readFileSync(basePath + '/' + currentDir + '/description');
-        repositories[currentDir] = description;
-        }
-    });
-    
-    
+            if (stats && stats.isDirectory()) {
+                var description = fs.readFileSync(basePath + '/' + currentDir + '/description');
+                repositories[currentDir] = description;
+            }
+        });
+ 
         res.render(
             'index',
             {
