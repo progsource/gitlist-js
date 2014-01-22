da2093d7920fa0de8061768e4d3588e4f5487efe
M. Petra Baranski <info@progsource.de>
2014-01-22 22:43:42 +0100

    commits controller - unix line endings, tabs->spaces

--- app/controller/CommitsController.js
+++ app/controller/CommitsController.js
@@ -4,56 +4,56 @@
 var CommitsController = function() {
     var getBootstrap = function() {
         var Bootstrap = require(__dirname + '/../Bootstrap.js');
-	return new Bootstrap().init();
+    return new Bootstrap().init();
     };
 
     var showCommits = function(req, res, basePath, reponame, branch, page) {
         var GitRepo = require(__dirname + '/../modules/GitRepo.js');
-	var gitRepo = new GitRepo();
-	
-	var GitLog = require(__dirname + '/../modules/GitLog.js');
-	var gitLog = new GitLog();
+    var gitRepo = new GitRepo();
+    
+    var GitLog = require(__dirname + '/../modules/GitLog.js');
+    var gitLog = new GitLog();
 
-	gitRepo.setCurrentReponame(reponame);
-	gitRepo.init();
+    gitRepo.setCurrentReponame(reponame);
+    gitRepo.init();
 
-	var branches = gitRepo.getBranches();
+    var branches = gitRepo.getBranches();
 
-	gitRepo.setCurrentBranch(branch);
+    gitRepo.setCurrentBranch(branch);
 
-	var renderIt = function(data) {
-	    res.render(
-		'logView',
-		{
-		    title: reponame,
-		    reponame: reponame,
-		    heads: branches.heads,
-		    tags: branches.tags,
-		    branch: branch,
-		    breadcrumb: data.breadcrumb,
-		    commits: data.commits,
-		    commitCount: data.commitCount,
-		    page: page,
-		    activeTab: 'Commits'
-		}
-	    );
-	};
+    var renderIt = function(data) {
+        res.render(
+        'logView',
+        {
+            title: reponame,
+            reponame: reponame,
+            heads: branches.heads,
+            tags: branches.tags,
+            branch: branch,
+            breadcrumb: data.breadcrumb,
+            commits: data.commits,
+            commitCount: data.commitCount,
+            page: page,
+            activeTab: 'Commits'
+        }
+        );
+    };
 
-	gitLog.getLog(basePath + '/' + reponame, branch, page, renderIt);
+    gitLog.getLog(basePath + '/' + reponame, branch, page, renderIt);
     };
 
     this.indexAction = function(req, res) {
         var basePath = getBootstrap().getBasePath();
-	var reponame = req.params.reponame;
-	var branch = req.params.branch;
+    var reponame = req.params.reponame;
+    var branch = req.params.branch;
 
-	if ('undefined' == typeof req.params.page) {
+    if ('undefined' == typeof req.params.page) {
             var page = 0;
         } else {
-	    var page = req.params.page;
-	}
+        var page = req.params.page;
+    }
 
-	showCommits(req, res, basePath, reponame, branch, page);
+    showCommits(req, res, basePath, reponame, branch, page);
     };
 };
 
