ead0b41c4479e32996afe50798f96c76fb974fd2
M. Petra Baranski <info@progsource.de>
2014-01-23 00:22:57 +0100

    jsdoc in GitData module

--- app/modules/GitData.js
+++ app/modules/GitData.js
@@ -1,45 +1,81 @@
 /**
+ * GitData
  *
+ * @file
+ * @license MIT
  */
+
+/** @ignore */
 var Promise = require('bluebird');
 
+/**
+ * @constructor
+ */
 var GitData = function() {
+    /**
+     * breadcrumb
+     *
+     * @type {object}
+     */
     var breadcrumb = {};
 
+    /**
+     * git tree line to object
+     *
+     * @param {string} line - one line of 'git ls-tree' output
+     * @param {object} folderObject - folder object
+     *
+     * @return {object} folder object
+     */
     var gitTreeLineToObject = function(line, folderObject) {
-    var key = line.split('\t')[1];
-    if ('undefined' !== typeof key) {
-        var contents = line.split('\t')[0].split(' ');
-        var contentName = key.split('/');
-        contentName = contentName[contentName.length - 1];
-        
-        folderObject[key] = {
-        mode: contents[0],
-        contentType: contents[1],
-        fileSize: contents[contents.length - 1],
-        contentName: contentName
-        };
-    }
-    return folderObject;
+        var key = line.split('\t')[1];
+        if ('undefined' !== typeof key) {
+            var contents = line.split('\t')[0].split(' ');
+            var contentName = key.split('/');
+            contentName = contentName[contentName.length - 1];
+
+            folderObject[key] = {
+                mode: contents[0],
+                contentType: contents[1],
+                fileSize: contents[contents.length - 1],
+                contentName: contentName
+            };
+        }
+        return folderObject;
     };
 
+    /**
+     * git tree string to object
+     *
+     * @param {object} data - String Buffer
+     *
+     * @return {object} breadcrumb and folder
+     */
     var gitTreeStringToObject = function(data) {
-    var lines = data.toString().split('\n');
-    var folder = {};
-    lines.forEach(function(line) {
-        gitTreeLineToObject(line, folder)
-    });
+        var lines = data.toString().split('\n');
+        var folder = {};
+        lines.forEach(function(line) {
+            gitTreeLineToObject(line, folder)
+        });
 
-    var tree = {
-        breadcrumb: breadcrumb,
-        folder: folder
+        var tree = {
+            breadcrumb: breadcrumb,
+            folder: folder
         };
-    return tree;
+        return tree;
     };
 
+    /**
+     * get folder
+     *
+     * @param {string} path - base path + repository name
+     * @param {string} branch - branch
+     * @param {string} deepPath - path to look at
+     * @param{function(data)} callback - callback that is called as soon as the folders are collected
+     */
     this.getFolder = function(path, branch, deepPath, callback) {
-    var spawn = require('child_process').spawn;
-    var prom = new Promise(function(resolve, reject) {
+        var spawn = require('child_process').spawn;
+        var prom = new Promise(function(resolve, reject) {
         var gitTree = spawn('git', ['ls-tree', branch, '-l', 'paths', deepPath], {cwd: path});
         gitTree.stdout.on('data', resolve);
         gitTree.stderr.on('data', reject);
@@ -48,13 +84,13 @@ var GitData = function() {
         breadcrumb.splice(breadcrumb.length - 1, 1);
         var breads = {};
         breadcrumb.forEach(function(bread, index) {
-        breads[bread] = '';
-        if (breadcrumb.length - 1 != index) {
-            var i = 0;
+            breads[bread] = '';
+            if (breadcrumb.length - 1 != index) {
+                var i = 0;
                 for (;i <= index; i++) {
-            breads[bread] += breadcrumb[i] + '%2F';
-                    }
-        }
+                    breads[bread] += breadcrumb[i] + '%2F';
+                }
+            }
         });
         breadcrumb = breads;
     })
