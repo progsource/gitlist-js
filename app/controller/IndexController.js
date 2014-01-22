7049a5eca87e09681c5853042b630b51c2c418c6
M. Petra Baranski <info@progsource.de>
2014-01-23 00:11:15 +0100

    line endings and tabs to spaces in index controller

--- app/controller/IndexController.js
+++ app/controller/IndexController.js
@@ -3,26 +3,26 @@
  */
 var IndexController = function() {
     var getBootstrap = function() {
-	var Bootstrap = require('./../Bootstrap.js');
-	return new Bootstrap().init();
+    var Bootstrap = require('./../Bootstrap.js');
+    return new Bootstrap().init();
     };
 
     this.indexAction = function(req, res) {
-	var fs = require('fs');
-	var repositories = {};
-	var basePath = getBootstrap().getBasePath();
-	var folder = fs.readdirSync(basePath);
+    var fs = require('fs');
+    var repositories = {};
+    var basePath = getBootstrap().getBasePath();
+    var folder = fs.readdirSync(basePath);
 
-	folder.forEach(function(currentDir) {
-	    var stats = fs.statSync(basePath + '/' + currentDir);
+    folder.forEach(function(currentDir) {
+        var stats = fs.statSync(basePath + '/' + currentDir);
 
-	    if (stats && stats.isDirectory()) {
-		var description = fs.readFileSync(basePath + '/' + currentDir + '/description');
-		repositories[currentDir] = description;
-	    }
-	});
-	
-	
+        if (stats && stats.isDirectory()) {
+        var description = fs.readFileSync(basePath + '/' + currentDir + '/description');
+        repositories[currentDir] = description;
+        }
+    });
+    
+    
         res.render(
             'index',
             {
