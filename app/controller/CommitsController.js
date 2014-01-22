76cd30bd03fe38df8d192a617fdc11e67639db53
M. Petra Baranski <info@progsource.de>
2014-01-22 23:58:02 +0100

    added jsdoc to Commits controller

--- app/controller/CommitsController.js
+++ app/controller/CommitsController.js
@@ -1,59 +1,87 @@
 /**
  * Commits Controller
+ *
+ * @file
+ * @license MIT
+ */
+
+/**
+ * @constructor
  */
 var CommitsController = function() {
+    /**
+     * get bootstrap
+     *
+     * @return {object} bootstrap
+     */
     var getBootstrap = function() {
         var Bootstrap = require(__dirname + '/../Bootstrap.js');
-    return new Bootstrap().init();
+        return new Bootstrap().init();
     };
 
+    /**
+     * show commits
+     *
+     * @param {object} req - request object
+     * @param {object} res - response object
+     * @param {string} basePath - base path
+     * @param {string} reponame - repository name
+     * @param {string} branch - branch
+     * @param {int} page - page number
+     */
     var showCommits = function(req, res, basePath, reponame, branch, page) {
         var GitRepo = require(__dirname + '/../modules/GitRepo.js');
-    var gitRepo = new GitRepo();
-    
-    var GitLog = require(__dirname + '/../modules/GitLog.js');
-    var gitLog = new GitLog();
+        var gitRepo = new GitRepo();
 
-    gitRepo.setCurrentReponame(reponame);
-    gitRepo.init();
+        var GitLog = require(__dirname + '/../modules/GitLog.js');
+        var gitLog = new GitLog();
 
-    var branches = gitRepo.getBranches();
+        gitRepo.setCurrentReponame(reponame);
+        gitRepo.init();
 
-    gitRepo.setCurrentBranch(branch);
+        var branches = gitRepo.getBranches();
 
-    var renderIt = function(data) {
-        res.render(
-        'logView',
-        {
-            title: reponame,
-            reponame: reponame,
-            heads: branches.heads,
-            tags: branches.tags,
-            branch: branch,
-            breadcrumb: data.breadcrumb,
-            commits: data.commits,
-            commitCount: data.commitCount,
-            page: page,
-            activeTab: 'Commits'
-        }
-        );
+        gitRepo.setCurrentBranch(branch);
+
+        var renderIt = function(data) {
+            res.render(
+                'logView',
+                {
+                    title: reponame,
+                    reponame: reponame,
+                    heads: branches.heads,
+                    tags: branches.tags,
+                    branch: branch,
+                    breadcrumb: data.breadcrumb,
+                    commits: data.commits,
+                    commitCount: data.commitCount,
+                    page: page,
+                    activeTab: 'Commits'
+                }
+            );
+        };
+
+        gitLog.getLog(basePath + '/' + reponame, branch, page, renderIt);
     };
 
-    gitLog.getLog(basePath + '/' + reponame, branch, page, renderIt);
-    };
-
+    /**
+     * index action
+     *
+     * @param {object} req - request object
+     * @param {object} res - response object
+     */
     this.indexAction = function(req, res) {
         var basePath = getBootstrap().getBasePath();
-    var reponame = req.params.reponame;
-    var branch = req.params.branch;
+        var reponame = req.params.reponame;
+        var branch = req.params.branch;
 
-    if ('undefined' == typeof req.params.page) {
+        if ('undefined' == typeof req.params.page) {
             var page = 0;
         } else {
-        var page = req.params.page;
-    }
+            var page = req.params.page;
+        }
 
-    showCommits(req, res, basePath, reponame, branch, page);
+        showCommits(req, res, basePath, reponame, branch, page);
     };
 };
 
