102b96fe5b6c132a3b66b150434476850f88b33e
M. Petra Baranski <info@progsource.de>
2014-01-22 22:22:54 +0100

    unix line endings in commit controller + jsdoc

--- app/controller/CommitController.js
+++ app/controller/CommitController.js
@@ -1,46 +1,60 @@
 /**
  * Commit Controller
+ *
+ * @file
+ * @license MIT
  */
 var CommitController = function() {
+    /**
+     * get bootstrap
+     *
+     * @return {object} bootstrap
+     */
     var getBootstrap = function() {
         var Bootstrap = require(__dirname + '/../Bootstrap.js');
-	return new Bootstrap().init();
+        return new Bootstrap().init();
     };
 
+    /**
+     * index action
+     *
+     * @param {object} req - request object
+     * @param {object} res - response object
+     */
     this.indexAction = function(req, res) {
         var basePath = getBootstrap().getBasePath();
         var reponame = req.params.reponame;
-	var treeish = req.params.treeish;
+        var treeish = req.params.treeish;
 
-	var GitRepo = require(__dirname + '/../modules/GitRepo.js');
-	var gitRepo = new GitRepo();
+        var GitRepo = require(__dirname + '/../modules/GitRepo.js');
+        var gitRepo = new GitRepo();
 
         var GitFile = require(__dirname + '/../modules/GitFile.js');
-	var gitFile = new GitFile();
+        var gitFile = new GitFile();
 
-	gitRepo.setCurrentReponame(reponame);
-	gitRepo.init();
+        gitRepo.setCurrentReponame(reponame);
+        gitRepo.init();
 
-	var branches = gitRepo.getBranches();
-	var branch = gitRepo.getCurrentBranch();
+        var branches = gitRepo.getBranches();
+        var branch = gitRepo.getCurrentBranch();
 
-	var renderIt = function(data) {
-	    res.render(
-		'fileView',
-		{
-		    title: reponame,
-		    reponame: reponame,
-		    heads: branches.heads,
-		    tags: branches.tags,
-		    branch: branch,
-		    breadcrumb: data.breadcrumb,
-		    fileContent: data.content,
-		    activeTab: 'Commits'
-		}
-	    );
-	};
+        var renderIt = function(data) {
+            res.render(
+                'fileView',
+                {
+                    title: reponame,
+                    reponame: reponame,
+                    heads: branches.heads,
+                    tags: branches.tags,
+                    branch: branch,
+                    breadcrumb: data.breadcrumb,
+                    fileContent: data.content,
+                    activeTab: 'Commits'
+                }
+            );
+        };
 
-	gitFile.getDiff(basePath + '/' + reponame, treeish, renderIt);
+        gitFile.getDiff(basePath + '/' + reponame, treeish, renderIt);
     };
 };
 
