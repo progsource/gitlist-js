0d570fccc57a810c18e8628a81f116677d1ee356
M. Petra Baranski <info@progsource.de>
2014-01-23 00:02:45 +0100

    file controller jsdoc

--- app/controller/FileController.js
+++ app/controller/FileController.js
@@ -1,48 +1,66 @@
 /**
  * File Controller
+ *
+ * @file
+ * @license MIT
+ */
+
+/**
+ * @constructor
  */
 var FileController = function() {
+    /**
+     * get bootstrap
+     *
+     * @return {object} bootstrap
+     */
     var getBootstrap = function() {
-    var Bootstrap = require(__dirname + '/../Bootstrap.js');
-    return new Bootstrap().init();
+        var Bootstrap = require(__dirname + '/../Bootstrap.js');
+        return new Bootstrap().init();
     };
 
+    /**
+     * index action
+     *
+     * @param {object} req - request object
+     * @param {object} res - response object
+     */
     this.indexAction = function(req, res) {
-    var basePath = getBootstrap().getBasePath();
-    var reponame = req.params.reponame;
-    var branch = req.params.branch;
-    var file = req.params.file;
+        var basePath = getBootstrap().getBasePath();
+        var reponame = req.params.reponame;
+        var branch = req.params.branch;
+        var file = req.params.file;
 
         var GitRepo = require(__dirname + '/../modules/GitRepo.js');
-    var gitRepo = new GitRepo();
+        var gitRepo = new GitRepo();
 
-    var GitFile = require(__dirname + '/../modules/GitFile.js');
-    var gitFile = new GitFile();
+        var GitFile = require(__dirname + '/../modules/GitFile.js');
+        var gitFile = new GitFile();
 
         gitRepo.setCurrentReponame(reponame);
-    gitRepo.init();
+        gitRepo.init();
     
-    var branches = gitRepo.getBranches();
+        var branches = gitRepo.getBranches();
 
-    gitRepo.setCurrentBranch(branch);
+        gitRepo.setCurrentBranch(branch);
 
-    var renderIt = function(data) {
-        res.render(
-        'fileView',
-        {
-            title: reponame,
-            reponame: reponame,
-            heads: branches.heads,
-            tags: branches.tags,
-            branch: branch,
-            fileContent: data.content,
-            breadcrumb: data.breadcrumb,
-            activeTab: 'Files'
-            }
-        );
-    };
+        var renderIt = function(data) {
+            res.render(
+                'fileView',
+                {
+                    title: reponame,
+                    reponame: reponame,
+                    heads: branches.heads,
+                    tags: branches.tags,
+                    branch: branch,
+                    fileContent: data.content,
+                    breadcrumb: data.breadcrumb,
+                    activeTab: 'Files'
+                }
+            );
+        };
 
-    gitFile.getFile(basePath + '/' + reponame, branch, file, renderIt);
+        gitFile.getFile(basePath + '/' + reponame, branch, file, renderIt);
     };
 };
 
